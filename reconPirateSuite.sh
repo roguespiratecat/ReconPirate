a63a041cc6543ea7064be49c4da50129d8dc924d
roguespiratecat <roguespiratecat@gmail.com>
2022-01-09 12:51:39 -0500

    Adding First cut Lazy Framework

--- reconPirateSuite.sh
+++ reconPirateSuite.sh
@@ -1,33 +1,45 @@
 
 
-wordList=$2
 
-if [[ -z $wordList ]] ; then
+wordList=$2
+outdir=recon_$1
+
+
+
+if [ -d "$outdir" ] ; then  
+	echo "[*] Work Space Allready Initialized !"
+	echo "[*] Please cd into $outdir and run ./tasks.sh"
+	exit 0
+fi
+
+echo "[*] Creating Creating new Workspace at $(pwd)/$outdir"
+
+if [ -z $wordList ] ; then
     echo '[*] No wordlist provided using default :\n/usr/share/amass/wordlists/deepmagic.com_top50kprefixes.txt '
     wordList=/usr/share/amass/wordlists/deepmagic.com_top50kprefixes.txt
 fi
+
 echo "[*] Creating Output Directories"
-outdir=recon_$1
 mkdir $outdir
+mkdir $outdir/content-discovery
+mkdir $outdir/screenshots
+mkdir $outdir/dorking
+
 echo ""
-echo "[*] Github Dorking across Repositories"
+echo "[*] Generating Github Dorks"
 echo ""
-./githubDorks.sh $1 > $outdir/githubDorks.txt
-cat $outdir/githubDorks.txt
+
+./scripts/githubDorks.sh $1 > $outdir/dorking/$1_githubDorks.txt
+
+cat $outdir/dorking/$1_githubDorks.txt
 echo ""
 echo "[*] Starting Amass Enumeration That may take a while"
 echo ""
 amass enum -active -d $1  -brute -w  $wordList  -dir $outdir/amass_$1_db -o $outdir/amass_$1.txt
 echo ""
-echo "[*] Probing active Web responses on subdomains"
-cat $outdir/amass_$1.txt | httprobe > $outdir/httprobe.txt
-cat $outdir/httprobe.txt
-echo "[*] Starting Initial Content Discovery on Discovered Domains"
+echo "[*] Finalizing Recon Setup"
+awk '{sub(/DOMAIN/,"test.ie")}1' tasks.sh > $outdir/tasks.sh
+chmod 777 $outdir/tasks.sh
+cp -r scripts/* $outdir
+echo "[*] Recon Setup Finished"
 
-readarray -t array < $outdir/amass_$1.txt
-mkdir $outdir/content-discovery
-for e in "${array[@]}"
-	./reconPirateDomainScrape "$e" > $outdir/content-discovery/"$e".txt
-do
-    
-done
---
+++ tasks.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+domain=DOMAIN
+
+
+httProbe ()  {
+	echo "[*] Probing active Web responses on subdomains"
+	cat ./amass_$1.txt | httprobe > ./httprobe.txt
+	cat ./httprobe.txt
+}
+
+siteCrawl () {
+	echo "[*] Starting Initial Content Discovery on Discovered Domains"
+	readarray -t array < ./amass_$1.txt
+	for e in "${array[@]}"
+	do
+		./reconPirateDomainScrape.sh "$e" > ./content-discovery/"$e".txt
+	done
+}
+
+amassEnumList () {
+	amass db -dir amass_$1_db -list
+}
+
+amassEnumShow () {
+	amass db -dir amass_$1_db -show 
+}
+
+massScan () {
+	./dnmasscan ./httprobe.txt dns.log -p80,443 -oG massScan.log
+}
+
+screenshotSites () {
+  echo "[*] Not Implemented"
+}
+
+generateGithubDorks ()  {
+	  ./githubDorks.sh $1 > ./dorking/$1-githubDorks.txt
+}
+
+
+#!/bin/bash
+# Bash Menu Script Example
+
+PS3='Recon Pirate Available Tasks: '
+options=("--Https-Probe" "--Content-Discovery" "--Amass-Enum-List" "--Amass-Enum-Show" "--Mass-Scan" "--Web-Screen-Shots" "--Git-Dorks" "--Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "--Https Probe")
+            echo "[*] Probing Http Servers "
+            httProbe $domain
+            cat ./httprobe.txt 
+            echo "[*] Probe Finished"
+            echo "[*] Results in $(pwd)/httprobe.txt"
+            exit 0
+            ;;
+        "--Content-Discovery")
+             echo "[*] Crawling Domains"
+             siteCrawl $domain
+             echo "[*] Process Finished"
+             echo "[*] Results in $(pwd)/content-discovery/"
+             ls content-discovery
+             exit 0
+            ;;
+        "--Amass-Enum-List")
+            amassEnumList $domain
+            ;;
+        "--Amass-Enum-Show")
+	        amassEnumShow $domain
+	        ;;
+        "--Mass-Scan")
+	         echo "[*] Starting Mass Scan"
+             massScan $domain
+             echo "[*] Process Finished"
+             echo "[*] Results in $(pwd)/massScan.log"
+             cat massScan.log
+             exit 0
+	        ;;
+        "--Web-Screen-Shots")
+	        eyewitness -f httprobe.txt  -d screenshots
+	        ;;
+        "--Git-Dorks")
+			echo "[*] Enter Domain Name "
+			read target   
+	        generateGithubDorks $target
+	        cat /dorking/$target-githubDorks.txt
+	        exit 0
+	        ;;    
+        "--Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
