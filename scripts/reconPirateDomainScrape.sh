773940c41e324fa21ab936fbb490b47acf5b2e46
roguespiratecat@gmail.com <root@6f1d00b98b8f>
2022-01-11 16:16:05 +0000

    Adding clean Up to domainn scraper

--- scripts/reconPirateDomainScrape.sh
+++ scripts/reconPirateDomainScrape.sh
@@ -1,6 +1,9 @@
 function ejs() {
    URL=$1
    curl -Lks $URL | tac | sed "s#\\\/#\/#g" | egrep -o "src['\"]?\s*[=:]\s*['\"]?[^'\"]+.js[^'\"> ]*" | sed -r "s/^src['\"]?[=:]['\"]//g" | awk -v url=$URL '{if(length($1)) if($1 ~/^http/) print $1; else if($1 ~/^\/\//) print "https:"$1; else print url"/"$1}' | sort -fu | xargs -I '%' sh -c "echo \"\n##### %\";wget --no-check-certificate --quiet \"%\";curl -Lks \"%\" | sed \"s/[;}\)>]/\n/g\" | grep -Po \"('#####.*)|(['\\\"](https?:)?[/]{1,2}[^'\\\"> ]{5,})|(\.(get|post|ajax|load)\s*\(\s*['\\\"](https?:)?[/]{1,2}[^'\\\"> ]{5,})\" | sort -fu" | tr -d "'\""
+
+   rm *.js
+   rm *js*
 }
 
 ejs $1
